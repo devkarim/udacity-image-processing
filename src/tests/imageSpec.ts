51b985de8f5b2814a49183de28fcaffec96ff341
devkarim <devkarim@hotmail.com>
2022-03-11 01:30:49 +0200

    unnecessary join methods

--- src/tests/imageSpec.ts
+++ src/tests/imageSpec.ts
@@ -1,10 +1,9 @@
 import { resizeSaveImage } from '../services/images';
-import path from 'path';
 
 describe('Test image utils', () => {
   it('should resize image and save', async () => {
-    const imagePath = path.join('assets/full/fjord.jpg');
-    const thumbPath = path.join('assets/test/fjord-test.png');
+    const imagePath = 'assets/full/fjord.jpg';
+    const thumbPath = 'assets/test/fjord-test.png';
 
     const promise = resizeSaveImage(imagePath, 200, 200, thumbPath, 'png');
     await expectAsync(promise).toBeResolved();

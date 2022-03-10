c55db08be0fc1d6fa7cd38a75d95c253e61c9a1f
devkarim <devkarim@hotmail.com>
2022-03-11 01:27:56 +0200

    added build to gitignore

--- build/config/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IMAGES_CACHE_DIR = exports.PORT = void 0;
-exports.PORT = 3000;
-exports.IMAGES_CACHE_DIR = 'assets/thumb';
--- build/helpers/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isFormatAllowed = void 0;
-var isFormatAllowed = function (x) {
-    return ['png', 'jpg', 'jpeg'].includes(x);
-};
-exports.isFormatAllowed = isFormatAllowed;
